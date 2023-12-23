a25ccfa456832a6e18de385e5363eec9a1b7d595
SabeelShaikh <81077584+SabeelShaikh@users.noreply.github.com>
2023-12-23 16:54:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// comment this is my first java script code
+
+console.log("hellow World");
+
+let name="sabeel";
+
+console.log(name);
+
+let firstName = "sabeel";
+
+let lastName = "shaikh";
+
+console.log(firstName + " "+ lastName);
